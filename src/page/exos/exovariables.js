b359ef802e0101f186589c066180ac7e32ecc6b3
NemesisRoss <NemesisRoss@hotmail.fr>
2025-10-15 14:10:35 +0200

    Initial commit

---
+++ src/page/exos/exovariables.js
@@ -0,0 +1,9 @@
+let int = 10;
+let float = 10.0000;
+let string = "dix";
+let bool = false;
+let array = [10, 10.0, "dix"];
+let object = new Object();
+object = { "int" : 10, "float" : 10.000};
+
+console.log( "integer = "+ int + "\nfloat = "+ float +"\nstring = "+ string + "\nbool = "+ bool + "\narray = "+ array + "\nobject = "+ object["int"]);
