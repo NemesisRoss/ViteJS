6b5a2081e9ff2d0513bec27ca1cc1ee28959b499
NemesisRoss <NemesisRoss@hotmail.fr>
2025-10-15 15:39:23 +0200

    exo pizza

--- src/page/exos/exovariables.js
+++ src/page/exos/exovariables.js
@@ -7,3 +7,18 @@ let object = new Object();
 object = { "int" : 10, "float" : 10.000};
 
 console.log( "integer = "+ int + "\nfloat = "+ float +"\nstring = "+ string + "\nbool = "+ bool + "\narray = "+ array + "\nobject = "+ object["int"]);
+
+let variété;
+let nomClient;
+let duréeLivraison;
+let adresse;
+let blagueNul;
+
+variété = "4 fromages";
+nomClient = "Dupont";
+duréeLivraison = 15;
+adresse = "10, rue des pizzas";
+blagueNul = `Qu'est qu'une pizza dit a un autre pizza qui lui demandait des conseils ?\n"Suis ta pâte et tout ira bien !"`;
+let sumUpPhrase = `Bonjour, Mr ${nomClient}, votre pizza: ${variété} est en cours de préparation.\nElle vous sera livrée d'ici ${duréeLivraison} minutes au ${adresse}.\nMerci d'avoir commandé chez la pizzeria "Raffinata"\n${blagueNul}`
+
+console.log(sumUpPhrase);
